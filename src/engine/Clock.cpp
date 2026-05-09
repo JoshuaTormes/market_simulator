bc747175d6498ffeeb42806aa17a7fd2eb938ddb
Joshua Sales <joshua.sales@houden.com.br>
2026-05-08 23:11:13 -0300

    wip: refactor

--- src/engine/Clock.cpp
+++
@@ -1,7 +0,0 @@
-#include "Clock.h"
-
-Clock::Clock(uint64_t maxTicks, double secondsPerTick)
-    : maxTicks(maxTicks), secondsPerTick(secondsPerTick), currentTick(0) {}
-
-uint64_t Clock::getCurrentTick() const { return currentTick; }
-double Clock::getSecondsPerTick() const { return secondsPerTick; }
